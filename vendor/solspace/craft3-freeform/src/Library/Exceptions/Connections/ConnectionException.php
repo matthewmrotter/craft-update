a1b677a1789c7339aa00836136288d5e325d4c32
matthewrotter <matthew@builtbymasonry.com>
2019-03-15 12:54:15 -0500

    no message

--- vendor/solspace/craft3-freeform/src/Library/Exceptions/Connections/ConnectionException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Solspace\Freeform\Library\Exceptions\Connections;
-
-use Solspace\Freeform\Library\Exceptions\FreeformException;
-
-class ConnectionException extends FreeformException
-{
-}
